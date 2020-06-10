9480314e19c3d0ee4d8e367218c0c16d0ba96aef
Valentín Giorgetti <58642296+ValentinGiorgetti@users.noreply.github.com>
2020-06-10 19:47:16 -0300

    ㅤ
    ㅤ

--- Tablero.py
+++ Tablero.py
@@ -3,6 +3,7 @@ from random import randint
 from pattern.es import parse, verbs, spelling, lexicon
 from collections import OrderedDict
 from itertools import permutations
+import sys
 
 def posicion_valida(event, posiciones_ocupadas, orientacion, posiciones_bloqueadas):
   if (not event in posiciones_bloqueadas):
@@ -60,9 +61,21 @@ def verificar_palabra(letras, posiciones_ocupadas, posiciones_bloqueadas, centro
       sg.Popup('Palabra inválida')
       return False
 
+def sumar_casilla(casillas_especiales, posicion, letra, puntos_jugada, multiplicador, posiciones_bloqueadas):
+  window.Element(posicion).Update(letra, button_color = ('white', 'red'))
+  puntos_jugada[0] += bolsa_de_fichas[letra]['puntaje_ficha']
+  posiciones_bloqueadas += [posicion]
+  if (posicion in casillas_especiales):
+    if (casillas_especiales[posicion]['modificador'] < 10):
+      puntos_jugada[0] += casillas_especiales[posicion]['modificador']
+    else:
+      multiplicador[0] += (casillas_especiales[posicion]['modificador'] % 10)
+
 def jugar_computadora(letras_pc, primer_jugada, centro, casillas_especiales, fichas_usadas_pc, posiciones_bloqueadas, bolsa_de_fichas):
   orientacion = random.randint(0, 100) % 2 == 0   # Si es par la orientacion es horizontal
   print(letras_pc)
+  puntos_jugada = [0]
+  multiplicador = [0]
   if (primer_jugada):
       i = centro[0]
       j = centro[1]
@@ -81,45 +94,25 @@ def jugar_computadora(letras_pc, primer_jugada, centro, casillas_especiales, fic
   if (encontre):
     print(palabra, es_palabra(palabra))
     if (primer_jugada):
-      if (orientacion):
-        for letra in palabra:
-          window.Element((i, j)).Update(letra, button_color = ('white', 'red'))
-          posiciones_bloqueadas += [(i,j)]
-          j += 1
-      else:
-        for letra in palabra:
-          window.Element((i, j)).Update(letra, button_color = ('white', 'red'))
-          posiciones_bloqueadas += [(i,j)]
-          i += 1
+      for letra in palabra:
+        sumar_casilla(casillas_especiales, (i,j), letra, puntos_jugada, multiplicador, posiciones_bloqueadas) 
+        j += 1 if orientacion else 0
+        i += 1 if not orientacion else 0
     else:
       posicion_valida = False
       while (not posicion_valida):
         posicion_valida = True
-        if (orientacion):
-          i = random.randint(0, FILAS - 1)
-          j = random.randint(0, COLUMNAS - 9)
-        else:
-          i = random.randint(0, FILAS - 9)
-          j = random.randint(0, COLUMNAS - 1)
+        i = random.randint(0, FILAS - 1) if orientacion else random.randint(0, FILAS - 9)
+        j = random.randint(0, COLUMNAS - 9) if orientacion else random.randint(0, COLUMNAS - 1)
         for x in range(len(palabra)):
-          if (orientacion):
-            if ((i, j + x) in posiciones_bloqueadas):
-              posicion_valida = False
-              break
-          else:
-            if ((i + x, j) in posiciones_bloqueadas):
-              posicion_valida = False
-              break
-      if (orientacion):
-        for letra in palabra:
-          window.Element((i, j)).Update(letra, button_color = ('white', 'red'))
-          posiciones_bloqueadas += [(i,j)]
-          j += 1
-      else:
-        for letra in palabra:
-          window.Element((i, j)).Update(letra, button_color = ('white', 'red'))
-          posiciones_bloqueadas += [(i,j)]
-          i += 1  
+          pos = (i, j + x) if orientacion else (i + x, j)
+          if (pos in posiciones_bloqueadas):
+            posicion_valida = False
+            break
+      for letra in palabra:
+        sumar_casilla(casillas_especiales, (i,j), letra, puntos_jugada, multiplicador, posiciones_bloqueadas) 
+        j += 1 if orientacion else 0
+        i += 1 if not orientacion else 0
     for letra in palabra:
       x = random.randint(8, 14)
       while (x in fichas_usadas_pc):
@@ -127,7 +120,11 @@ def jugar_computadora(letras_pc, primer_jugada, centro, casillas_especiales, fic
       window.Element(x).Update(button_color = ('white', 'red'), disabled = True)
       fichas_usadas_pc += [x]
       letras_pc.remove(letra)
-    return True
+    print(puntos_jugada[0], '*', multiplicador[0])
+    if (puntos_jugada[0] < 0):
+      return 0
+    else:
+      return puntos_jugada[0] if multiplicador[0] == 0 else puntos_jugada[0] * multiplicador[0]
   else:
     abecedario = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
     letras_pc.clear()
@@ -139,42 +136,58 @@ def jugar_computadora(letras_pc, primer_jugada, centro, casillas_especiales, fic
       while (bolsa_de_fichas[letra] == 0):
         letra = random.choice(abecedario)
       letras_pc += [letra]
-      bolsa_de_fichas[letra] -= 1
+      bolsa_de_fichas[letra]['cantidad_fichas'] -= 1
     print('nuevas letras', letras_pc)
-    return False
+    return 0
 
 def colocar_fichas_especiales(nivel, casillas_especiales, FILAS, COLUMNAS):
   malas_NivelFacil =[(1,7),(1,11),(2,8),(2,10),(3,9),
-                   (7,1),(11,1),(8,2),(10,2),(9,3),
-                   (17,7),(11,17),(8,16),(10,16),(9,15),
-                   (7,17),(17,11),(16,18),(16,10),(15,9)]
+                   (17,7),(11,17),(8,16),(10,16),(9,15)]
 
-  malas_NivelMedio=[(2,7),(2,9),(3,8),(4,7),(4,9),
-                    (7,2),(9,4),(8,3),(7,4),(9,2),
-                    (7,14),(7,12),(9,14),(9,12),(8,13),
-                    (14,7),(12,7),(14,9),(12,9),(13,8)]
+  malas_NivelMedio=[(3,7),(3,9),(4,8),(5,7),(5,9),
+                    (7,13),(7,11),(9,13),(9,11),(8,12)]
+
+  pal_NivelMedio=[(0,8),(1,7),(1,9),(2,6),(2,10),(3,5),(3,11),
+                  (8,16),(7,15),(9,15),(6,14),(10,14),(5,13),(11,13)
+
+  ]
 
   malas_NivelDificil=[(0,5),(0,9),(1,6),(1,8),(2,7),(3,6),(3,8),(4,7),
-                      (5,0),(9,0),(6,1),(8,1),(7,2),(6,3),(8,3),(7,4),
-                      (14,5),(14,9),(13,6),(13,8),(12,7),(11,6),(11,8),(10,7),
-                      (5,14),(9,14),(6,13),(8,13),(7,12),(6,11),(8,11),(7,10)]
+                      (14,5),(14,9),(13,6),(13,8),(12,7),(11,6),(11,8),(10,7)]
+
+  mala_actual = -1
 
   for i in range(FILAS):
     for j in range(COLUMNAS):
       tupla=(i,j)
+      tuplain=(j,i)
       if(tupla!=centro):
-        if(j==i or i+j==FILAS-1):
-          tablero_juego[i][j] = sg.Button('Buena', size = (4, 2), key = (i, j), pad = (0.5, 0.5), button_color = ('white', 'blue'))
-          casillas_especiales[tupla] = {'color' : ('white', 'blue'), 'texto' : 'Buena'}
-        if (nivel == 1) and (tupla in malas_NivelFacil):
-          tablero_juego[i][j]=sg.Button('Mala', size=(4, 2), key=(i, j), pad=(0.5, 0.5), button_color=('white', 'black'))
-          casillas_especiales[tupla] = {'color' : ('white', 'black'), 'texto' : 'Mala'}
-        elif (nivel == 2) and (tupla in malas_NivelMedio):
-          tablero_juego[i][j]=sg.Button('Mala', size=(4, 2), key=(i, j), pad=(0.5, 0.5), button_color=('white', 'black'))
-          casillas_especiales[tupla]={'color': ('white', 'black'), 'texto': 'Mala'}
-        elif (nivel== 3) and (tupla in malas_NivelDificil):
-          tablero_juego[i][j]=sg.Button('Mala', size=(4, 2), key=(i, j), pad=(0.5, 0.5), button_color=('white', 'black'))
-          casillas_especiales[tupla]={'color': ('white', 'black'), 'texto': 'Mala'}
+        if(j==i):
+          tablero_juego[i][j] = sg.Button('F +2', size = (4, 2), key = (i, j), pad = (0.5, 0.5), button_color = ('white', 'blue'))
+          casillas_especiales[(i,j)] = {'color' : ('white', 'blue'), 'modificador' : 2}
+        else:
+          if(i + j==FILAS - 1):
+            tablero_juego[i][j]=sg.Button('F +3', size=(4, 2), key=(i, j), pad=(0.5, 0.5),button_color=('white', 'blue'))
+            casillas_especiales[(i, j)]={'color': ('white', 'blue'), 'modificador' : 3}
+        if ((nivel == 1) and ((tupla in malas_NivelFacil)or(tuplain in malas_NivelFacil))):
+          tablero_juego[i][j]=sg.Button('F ' + str(mala_actual), size=(4, 2), key=(i, j), pad=(0.5, 0.5), button_color=('white', 'black'))
+          casillas_especiales[(i, j)] = {'color' : ('white', 'black'), 'modificador' : mala_actual}
+          mala_actual = mala_actual - 1 if mala_actual > -3 else -1
+        elif (nivel == 2):
+          if((tupla in malas_NivelMedio) or (tuplain in malas_NivelMedio)):
+            tablero_juego[i][j]=sg.Button('F ' + str(mala_actual), size=(4, 2), key=(i, j), pad=(0.5, 0.5), button_color=('white', 'black'))
+            casillas_especiales[(i, j)] = {'color' : ('white', 'black'), 'modificador' : mala_actual}
+            mala_actual = mala_actual - 1 if mala_actual > -3 else -1
+          if(tupla in pal_NivelMedio):
+            tablero_juego[i][j]=sg.Button('P x3', size=(4, 2), key=(i, j), pad=(0.5, 0.5),button_color=('white', 'purple'))
+            casillas_especiales[(i, j)]={'color': ('white', 'black'), 'modificador' : 13}
+          if(tuplain in pal_NivelMedio):
+            tablero_juego[i][j]=sg.Button('P x2', size=(4, 2), key=(i, j), pad=(0.5, 0.5),button_color=('white', 'purple'))
+            casillas_especiales[(i, j)]={'color': ('white', 'black'), 'modificador' : 12}
+        elif (nivel== 3) and ((tupla in malas_NivelDificil) or (tuplain in malas_NivelDificil)):
+          tablero_juego[i][j]=sg.Button('F ' + str(mala_actual), size=(4, 2), key=(i, j), pad=(0.5, 0.5), button_color=('white', 'black'))
+          casillas_especiales[(i, j)] = {'color' : ('white', 'black'), 'modificador' : mala_actual}
+          mala_actual = mala_actual - 1 if mala_actual > -3 else -1
 
 def agregar_posiciones_bloqueadas(posiciones_ocupadas, posiciones_bloqueadas):
   for posicion in posiciones_ocupadas:
@@ -185,38 +198,35 @@ def repartir_fichas(bolsa_de_fichas, letras_jugador, letras_pc):
   for i in range(7):
     letra = random.choice(abecedario)
     letras_jugador += [letra]
-    bolsa_de_fichas[letra] -= 1
+    bolsa_de_fichas[letra]['cantidad_fichas'] -= 1
     letra = random.choice(abecedario)
     letras_pc += [letra]
-    bolsa_de_fichas[letra] -= 1
+    bolsa_de_fichas[letra]['cantidad_fichas'] -= 1
 
-FILAS = COLUMNAS = 15
+FILAS = COLUMNAS = 17
+nivel = 2
 
 centro = (int(FILAS / 2), int(FILAS / 2))
 
-casillas_buenas = 3
-casillas_malas = 5
+abecedario = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
-palabras_validas = {'adjetivos' : ("AO", "JJ","AQ","DI","DT"), 
-                    'sustantivos': ("NC", "NN", "NCS","NCP", "NNS","NP", "NNP","W"), 
-                    'verbos': ("VAG", "VBG", "VAI","VAN", "MD", "VAS" , "VMG" , "VMI", "VB", "VMM" ,"VMN" , "VMP", "VBN","VMS","VSG",  "VSI","VSN", "VSP","VSS")}
+bolsa_de_fichas = {}
 
-bolsa_de_fichas = {'A' : 10, 'B' : 10, 'C' : 10, 'D' : 10, 'E' : 10, 'F' : 10, 'G' : 10, 'H' : 10, 'I' : 10, 'J' : 10, 'K' : 10, 'L' : 10, 'M' : 10, 'N' : 10, 'O' : 10, 'P' : 10, 'Q' : 10, 'R' : 10, 'S' : 10, 'T' : 10, 'U' : 10, 'V' : 10, 'W' : 10, 'X' : 10, 'Y' : 10, 'Z' : 10}
+for letra in abecedario:
+  bolsa_de_fichas[letra] = {'cantidad_fichas' : 10, 'puntaje_ficha' : 1}
 
 letras_jugador = []
 letras_pc = []
 
 repartir_fichas(bolsa_de_fichas, letras_jugador, letras_pc)
 
-print(bolsa_de_fichas)
-
 tablero_juego = [[sg.Button('', size = (4, 2), key = (i, j), pad = (0.5, 0.5), button_color = ('white', 'green')) for j in range(COLUMNAS)] for i in range(FILAS)]
 
 tablero_juego[centro[0]][centro[1]] = sg.Button('Inicio', size = (4, 2), key = (centro[0], centro[1]), pad = (0.5, 0.5), button_color = ('white', 'yellow'))
 
-casillas_especiales = {centro : {'color' : ('white', 'yellow'), 'texto' : 'Inicio'}}
+casillas_especiales = {centro : {'color' : ('white', 'yellow'), 'modificador' : 1}}
 
-colocar_fichas_especiales(3, casillas_especiales, FILAS, COLUMNAS)
+colocar_fichas_especiales(nivel, casillas_especiales, FILAS, COLUMNAS)
 
 fichas_jugador = [sg.Button(letras_jugador[i], size= (4, 2), key = i, pad = (0.5, 0.5), button_color = ('white', 'green')) for i in range(7)]
 
@@ -230,8 +240,8 @@ columna1 = layout
 
 columna2 = [[sg.Text('Tiempo restante'), sg.Text(' ', key = 'tiempo')],
             [sg.Text('Puntajes')],
-            [sg.Text('Jugador'), sg.Text(' ', key = 'puntaje_jugador')],
-            [sg.Text('Computadora'), sg.Text(' ', key = 'puntaje_computadora')],
+            [sg.Text('Jugador'), sg.Text('0       ', key = 'puntaje_jugador')],
+            [sg.Text('Computadora'), sg.Text('0     ', key = 'puntaje_computadora')],
             [sg.Text('Turno actual'), sg.Text('                  ', key = 'turno')],
             [sg.Text('Palabra actual'), sg.Text('                 ', key = 'palabra_actual')],
             [sg.Button('Confirmar palabra', key = 'confirmar'), sg.Button('Cambiar fichas', key = 'cambiar'), sg.Button('Pasar', key = 'pasar')],
@@ -241,15 +251,15 @@ layout = [[sg.Column(columna1), sg.Column(columna2)]]
 
 window = sg.Window('Tablero', layout)
 
-abecedario = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ') # 
+abecedario = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ') 
 
-layout_cambiar_fichas = [[sg.Button('Cambiar todas', key = 'todas'), sg.Button('Cambiar algunas', key = 'algunas')],
-                         [sg.Text('Seleccione las fichas que quiere intercambiar', key = 'texto1', visible = False)],
-                         [sg.Text('Fichas en la bolsa', key = 'texto2', visible = False)],
-                         [sg.Button(abecedario[i] + ' x' + str(bolsa_de_fichas[abecedario[i]]), size= (4, 2), key = abecedario[i], pad = (0.5, 0.5), button_color = ('white', 'green')) for i in range(14)],
-                         [sg.Button(abecedario[i] + ' x' + str(bolsa_de_fichas[abecedario[i]]), size= (4, 2), key = i, pad = (0.5, 0.5), button_color = ('white', 'green')) for i in range(14,26)]]
+# layout_cambiar_fichas = [[sg.Button('Cambiar todas', key = 'todas'), sg.Button('Cambiar algunas', key = 'algunas')],
+#                          [sg.Text('Seleccione las fichas que quiere intercambiar', key = 'texto1', visible = False)],
+#                          [sg.Text('Fichas en la bolsa', key = 'texto2', visible = False)],
+#                          [sg.Button(abecedario[i] + ' x' + str(bolsa_de_fichas[abecedario[i]]), size= (4, 2), key = abecedario[i], pad = (0.5, 0.5), button_color = ('white', 'green')) for i in range(14)],
+#                          [sg.Button(abecedario[i] + ' x' + str(bolsa_de_fichas[abecedario[i]]), size= (4, 2), key = i, pad = (0.5, 0.5), button_color = ('white', 'green')) for i in range(14,26)]]
 
-ventana_cambiar_fichas = sg.Window('Cambiar fichas', layout_cambiar_fichas)
+# ventana_cambiar_fichas = sg.Window('Cambiar fichas', layout_cambiar_fichas)
 
 # ventana_cambiar_fichas.Read()
 # ventana_cambiar_fichas.Close()
@@ -263,15 +273,19 @@ primer_jugada = True
 turno_jugador = random.randint(0, 100) % 2 == 0 #   si el número aleatorio es par, comienza el jugador
 posiciones_bloqueadas = []
 fichas_usadas_pc = []
-
-# print('palabritas', es_palabra('su'), es_palabra('las'))
+puntaje_pc = 0
+puntaje_jugador = 0
 
 while True:
   event, values = window.Read()
   if (not turno_jugador):
-    if (jugar_computadora(letras_pc, primer_jugada, centro, casillas_especiales, fichas_usadas_pc, posiciones_bloqueadas, bolsa_de_fichas)):
+    jugada = jugar_computadora(letras_pc, primer_jugada, centro, casillas_especiales, fichas_usadas_pc, posiciones_bloqueadas, bolsa_de_fichas)
+    if (jugada > 0):
+      puntaje_pc += jugada
+      window.Element('puntaje_computadora').Update(puntaje_pc)
       primer_jugada = False
     turno_jugador = True
+    print(puntaje_pc)
   if (event in (None, 'Terminar')):
       break
   if (event == 'cambiar'):
