5cc5b7e7c896018f9e167f5633349ee39f8f0180
Valentín Giorgetti <58642296+ValentinGiorgetti@users.noreply.github.com>
2020-06-10 22:13:07 -0300

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -77,8 +77,8 @@ def jugar_computadora(letras_pc, primer_jugada, centro, casillas_especiales, fic
   puntos_jugada = [0]
   multiplicador = [0]
   if (primer_jugada):
-      i = centro[0]
-      j = centro[1]
+    i = centro[0]
+    j = centro[1]
   permutaciones = permutations(letras_pc)
   encontre = False
   for permutacion in permutaciones:
@@ -86,7 +86,7 @@ def jugar_computadora(letras_pc, primer_jugada, centro, casillas_especiales, fic
       break
     temp = ''.join(permutacion)
     for x in range(7, 1, -1):
-      palabra = temp[0:x + 1]
+      palabra = temp[0 : x + 1]
       if (es_palabra(palabra)):
         encontre = True
         break
@@ -95,7 +95,7 @@ def jugar_computadora(letras_pc, primer_jugada, centro, casillas_especiales, fic
     print(palabra, es_palabra(palabra))
     if (primer_jugada):
       for letra in palabra:
-        sumar_casilla(casillas_especiales, (i,j), letra, puntos_jugada, multiplicador, posiciones_bloqueadas) 
+        sumar_casilla(casillas_especiales, (i, j), letra, puntos_jugada, multiplicador, posiciones_bloqueadas) 
         j += 1 if orientacion else 0
         i += 1 if not orientacion else 0
     else:
@@ -110,7 +110,7 @@ def jugar_computadora(letras_pc, primer_jugada, centro, casillas_especiales, fic
             posicion_valida = False
             break
       for letra in palabra:
-        sumar_casilla(casillas_especiales, (i,j), letra, puntos_jugada, multiplicador, posiciones_bloqueadas) 
+        sumar_casilla(casillas_especiales, (i, j), letra, puntos_jugada, multiplicador, posiciones_bloqueadas) 
         j += 1 if orientacion else 0
         i += 1 if not orientacion else 0
     for letra in palabra:
@@ -140,54 +140,57 @@ def jugar_computadora(letras_pc, primer_jugada, centro, casillas_especiales, fic
     print('nuevas letras', letras_pc)
     return 0
 
-def colocar_fichas_especiales(nivel, casillas_especiales, FILAS, COLUMNAS):
-  malas_NivelFacil =[(1,7),(1,11),(2,8),(2,10),(3,9),
-                   (17,7),(11,17),(8,16),(10,16),(9,15)]
-
-  malas_NivelMedio=[(3,7),(3,9),(4,8),(5,7),(5,9),
-                    (7,13),(7,11),(9,13),(9,11),(8,12)]
-
-  pal_NivelMedio=[(0,8),(1,7),(1,9),(2,6),(2,10),(3,5),(3,11),
-                  (8,16),(7,15),(9,15),(6,14),(10,14),(5,13),(11,13)
-
-  ]
-
-  malas_NivelDificil=[(0,5),(0,9),(1,6),(1,8),(2,7),(3,6),(3,8),(4,7),
-                      (14,5),(14,9),(13,6),(13,8),(12,7),(11,6),(11,8),(10,7)]
+def colocar_posiciones_especiales(nivel, casillas_especiales, FILAS, COLUMNAS, centro):
+  malas_nivel_facil =[(4, 8), (5, 9), (4, 10), (3, 9), (8, 14), (9, 13), (10, 14), (9, 15)]
+  #malas_nivel_facil =[(1, 7), (1, 11), (2, 8),(2, 10), (3, 9), (17, 7), (11, 17), (8, 16), (10, 16), (9, 15)]
+  multiplicador_nivel_facil = [(0,9),(1,8),(1,10),(2,7),(2,11),(3,6),(3,12),(4,5),(4,13), (9,18),(8,17),(10,17),(7,16),(11,16),(6,15),(12,15),(5,14),(13,14)]
+  
+  malas_nivel_medio = [(3, 7), (3, 9), (4, 8), (5, 7), (5, 9), (7, 13), (7, 11), (9, 13), (9, 11), (8, 12)]
+  multiplicador_nivel_medio = [(0, 8), (1, 7), (1, 9), (2, 6), (2, 10), (3, 5), (3, 11), (8, 16), (7, 15), (9, 15), (6, 14), (10, 14), (5, 13), (11, 13)]
+  
+  malas_nivel_dificil=[(0,7),(1,6),(1,8),(2,5),(2,9),(3,4),(3,10),(7,14),(6,13),(8,13),(5,12),(9,12),(4,11),(10,11)]
+  #malas_nivel_dificil = [(0, 5), (0, 9), (1, 6), (1, 8), (2, 7), (3, 6), (3, 8), (4, 7), (14, 5), (14, 9), (13, 6), (13, 8), (12, 7), (11, 6), (11, 8), (10, 7)]
+  multiplicador_nivel_dificil = [(2,7),(3,6),(3,8),(7,12),(6,11),(8,11)]
 
   mala_actual = -1
 
   for i in range(FILAS):
     for j in range(COLUMNAS):
-      tupla=(i,j)
-      tuplain=(j,i)
-      if(tupla!=centro):
-        if(j==i):
-          tablero_juego[i][j] = sg.Button('F +2', size = (4, 2), key = (i, j), pad = (0.5, 0.5), button_color = ('white', 'blue'))
-          casillas_especiales[(i,j)] = {'color' : ('white', 'blue'), 'modificador' : 2}
-        else:
-          if(i + j==FILAS - 1):
-            tablero_juego[i][j]=sg.Button('F +3', size=(4, 2), key=(i, j), pad=(0.5, 0.5),button_color=('white', 'blue'))
-            casillas_especiales[(i, j)]={'color': ('white', 'blue'), 'modificador' : 3}
-        if ((nivel == 1) and ((tupla in malas_NivelFacil)or(tuplain in malas_NivelFacil))):
-          tablero_juego[i][j]=sg.Button('F ' + str(mala_actual), size=(4, 2), key=(i, j), pad=(0.5, 0.5), button_color=('white', 'black'))
+      posicion = (i, j)
+      posicion_invertida=(j,i)
+      if(j == i):
+        tablero_juego[i][j] = sg.Button('F +2', size = (4, 2), key = (i, j), pad = (0.5, 0.5), button_color = ('white', 'blue'))
+        casillas_especiales[(i, j)] = {'color' : ('white', 'blue'), 'modificador' : 2}
+      elif(i + j == FILAS - 1):
+        tablero_juego[i][j] = sg.Button('F +3', size = (4, 2), key = (i, j), pad = (0.5, 0.5), button_color = ('white', 'blue'))
+        casillas_especiales[(i, j)] = {'color': ('white', 'blue'), 'modificador' : 3}
+      elif (nivel == 1):
+        if ((posicion in malas_nivel_facil) or (posicion_invertida in malas_nivel_facil)):
+          tablero_juego[i][j] = sg.Button('F ' + str(mala_actual), size = (4, 2), key = (i, j), pad = (0.5, 0.5), button_color = ('white', 'black'))
           casillas_especiales[(i, j)] = {'color' : ('white', 'black'), 'modificador' : mala_actual}
           mala_actual = mala_actual - 1 if mala_actual > -3 else -1
-        elif (nivel == 2):
-          if((tupla in malas_NivelMedio) or (tuplain in malas_NivelMedio)):
-            tablero_juego[i][j]=sg.Button('F ' + str(mala_actual), size=(4, 2), key=(i, j), pad=(0.5, 0.5), button_color=('white', 'black'))
-            casillas_especiales[(i, j)] = {'color' : ('white', 'black'), 'modificador' : mala_actual}
-            mala_actual = mala_actual - 1 if mala_actual > -3 else -1
-          if(tupla in pal_NivelMedio):
-            tablero_juego[i][j]=sg.Button('P x3', size=(4, 2), key=(i, j), pad=(0.5, 0.5),button_color=('white', 'purple'))
-            casillas_especiales[(i, j)]={'color': ('white', 'black'), 'modificador' : 13}
-          if(tuplain in pal_NivelMedio):
-            tablero_juego[i][j]=sg.Button('P x2', size=(4, 2), key=(i, j), pad=(0.5, 0.5),button_color=('white', 'purple'))
-            casillas_especiales[(i, j)]={'color': ('white', 'black'), 'modificador' : 12}
-        elif (nivel== 3) and ((tupla in malas_NivelDificil) or (tuplain in malas_NivelDificil)):
-          tablero_juego[i][j]=sg.Button('F ' + str(mala_actual), size=(4, 2), key=(i, j), pad=(0.5, 0.5), button_color=('white', 'black'))
+        elif ((posicion in multiplicador_nivel_facil) or (posicion_invertida in multiplicador_nivel_facil)):
+          tablero_juego[i][j] = sg.Button('P x3' if posicion in multiplicador_nivel_facil else 'P x2', size = (4, 2), key = (i, j), pad = (0.5, 0.5), button_color = ('white', 'purple'))
+          casillas_especiales[(i, j)]={'color': ('white', 'purple'), 'modificador' : 13 if posicion in multiplicador_nivel_facil else 12}
+      elif (nivel == 2):
+        if ((posicion in malas_nivel_medio) or (posicion_invertida in malas_nivel_medio)):
+          tablero_juego[i][j] = sg.Button('F ' + str(mala_actual), size = (4, 2), key = (i, j), pad = (0.5, 0.5), button_color = ('white', 'black'))
           casillas_especiales[(i, j)] = {'color' : ('white', 'black'), 'modificador' : mala_actual}
           mala_actual = mala_actual - 1 if mala_actual > -3 else -1
+        elif (posicion in multiplicador_nivel_medio or posicion_invertida in multiplicador_nivel_medio):
+          tablero_juego[i][j] = sg.Button('P x3' if posicion_invertida in multiplicador_nivel_medio else 'P x2', size = (4, 2), key = (i, j), pad = (0.5, 0.5), button_color = ('white', 'purple'))
+          casillas_especiales[(i, j)]={'color': ('white', 'purple'), 'modificador' : 13 if posicion_invertida in multiplicador_nivel_medio else 12}
+      elif (nivel == 3):
+        if ((posicion in malas_nivel_dificil) or (posicion_invertida in malas_nivel_dificil)):
+          tablero_juego[i][j] = sg.Button('F ' + str(mala_actual), size = (4, 2), key = (i, j), pad = (0.5, 0.5), button_color = ('white', 'black'))
+          casillas_especiales[(i, j)] = {'color' : ('white', 'black'), 'modificador' : mala_actual}
+          mala_actual = mala_actual - 1 if mala_actual > -3 else -1
+        elif (posicion in multiplicador_nivel_dificil or posicion_invertida in multiplicador_nivel_dificil):
+          tablero_juego[i][j] = sg.Button('P x3' if posicion in multiplicador_nivel_dificil else 'P x2', size = (4, 2), key = (i, j), pad = (0.5, 0.5), button_color = ('white', 'purple'))
+          casillas_especiales[(i, j)]={'color': ('white', 'purple'), 'modificador' : 13 if posicion in multiplicador_nivel_dificil else 12}
+          
+  tablero_juego[centro[0]][centro[1]] = sg.Button('Inicio', size = (4, 2), key = (centro[0], centro[1]), pad = (0.5, 0.5), button_color = ('white', 'yellow'))
+  casillas_especiales[(centro[0], centro[1])] = {'color' : ('white', 'yellow'), 'modificador' : 1}        
 
 def agregar_posiciones_bloqueadas(posiciones_ocupadas, posiciones_bloqueadas):
   for posicion in posiciones_ocupadas:
@@ -203,8 +206,8 @@ def repartir_fichas(bolsa_de_fichas, letras_jugador, letras_pc):
     letras_pc += [letra]
     bolsa_de_fichas[letra]['cantidad_fichas'] -= 1
 
-FILAS = COLUMNAS = 17
 nivel = 2
+FILAS = COLUMNAS = 17
 
 centro = (int(FILAS / 2), int(FILAS / 2))
 
@@ -222,11 +225,8 @@ repartir_fichas(bolsa_de_fichas, letras_jugador, letras_pc)
 
 tablero_juego = [[sg.Button('', size = (4, 2), key = (i, j), pad = (0.5, 0.5), button_color = ('white', 'green')) for j in range(COLUMNAS)] for i in range(FILAS)]
 
-tablero_juego[centro[0]][centro[1]] = sg.Button('Inicio', size = (4, 2), key = (centro[0], centro[1]), pad = (0.5, 0.5), button_color = ('white', 'yellow'))
-
-casillas_especiales = {centro : {'color' : ('white', 'yellow'), 'modificador' : 1}}
-
-colocar_fichas_especiales(nivel, casillas_especiales, FILAS, COLUMNAS)
+casillas_especiales = {}
+colocar_posiciones_especiales(nivel, casillas_especiales, FILAS, COLUMNAS, centro)
 
 fichas_jugador = [sg.Button(letras_jugador[i], size= (4, 2), key = i, pad = (0.5, 0.5), button_color = ('white', 'green')) for i in range(7)]
 
