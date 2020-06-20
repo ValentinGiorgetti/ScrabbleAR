0bda36364b8a6e1050c3d47062bb54568b78bbbf
Valentín Giorgetti <58642296+ValentinGiorgetti@users.noreply.github.com>
2020-06-19 21:12:46 -0300

    ㅤ

---
+++ Jugador.py
@@ -0,0 +1,17 @@
+class Jugador:
+    
+  def __init__(self):
+    self._puntaje = 0
+    self._cambios_restantes = 3
+
+  def get_puntaje(self):
+    return self._puntaje
+
+  def actualizar_puntaje(self, puntos):
+    self._puntaje += puntos
+
+  def get_cambios_restantes(self):
+    return self._cambios_restantes
+
+  def actualizar_cambios_restantes(self):
+    self._cambios_restantes -= 1
