62dfa79f4c6a3268d777479b7b6b7deb891d92fd
Valentín Giorgetti <58642296+ValentinGiorgetti@users.noreply.github.com>
2020-07-23 12:23:18 -0300

    Archivo principal

--- ScrabbleAR.py
+++ ScrabbleAR.py
@@ -3,6 +3,7 @@ from componentes.tablero import jugar
 from componentes.jugador import Jugador
 import random, pickle, json, PySimpleGUI as sg
 from os.path import join
+from playsound import playsound as reproducir 
 
 def main():
     """
@@ -24,59 +25,66 @@ def main():
         partida = pickle.load(f)
 
     layout = [
-        [sg.Text("ScrabbleAR")],
-        [sg.Button("Imagen scrabble", disabled=True)],
-        [sg.Button("Configuración", key=configuracion), sg.Button("Ver reglas del juego", key=reglas), sg.Button("Ver top de puntajes", key=top_puntajes)],
-        [sg.Button("Reanudar partida", size=(7, 2), key="reanudar", disabled=partida == None,), sg.Button("Iniciar nueva partida", size=(11, 2), key=jugar)],
-        [sg.Button("Salir")]
+        [sg.Text("")],
+        [sg.Image(join('componentes', 'imagenes', 'logo.png'))],
+        [sg.Text("")],
+        [sg.Button("Configuración", size = (25, 1), key=configuracion)],
+        [sg.Button("Ver reglas del juego", size = (25, 1), key=reglas)],
+        [sg.Button("Ver top de puntajes", size = (25, 1), key=top_puntajes)],
+        [sg.Button("Reanudar partida", size = (25, 1), key="reanudar", disabled=partida == None,)],
+        [sg.Button("Iniciar nueva partida", size = (25, 1), key=jugar)],
+        [sg.Button("Salir", size = (25, 1))],
+        [sg.Text("")],
     ]
-    ventana = sg.Window("ScrabbleAR", layout)
+    ventana = sg.Window("ScrabbleAR", layout, element_justification = 'center', auto_size_text=True, auto_size_buttons=True, finalize = True, use_default_focus = False, text_justification = 'center', disable_close = True)
 
     layout_confirmar = [
         [sg.Text("Hay una partida guardada, si inicia una nueva no podrá continuar con la anterior")],
         [sg.Button("Cancelar"), sg.Button("Continuar")],
     ]
-    window_confirmar = sg.Window("Partida nueva", layout_confirmar)
+    window_confirmar = sg.Window("Partida nueva", layout_confirmar, element_justification = 'center', auto_size_text=True, auto_size_buttons=True, finalize = True)
+    window_confirmar.Hide()
 
     with open(join("componentes", "informacion_guardada", "ultima_configuracion.json")) as f:
         configuracion_seleccionada = json.load(f)
-
-    with open(join("componentes", "informacion_guardada", "top_puntajes"), "rb") as f:
-        top = pickle.load(f)
-
-    ok = True
+        
+    partida_pospuesta = False
+    opcion = 'Continuar'
 
     while True:
         event = ventana.Read()[0]
+        reproducir(join("componentes", "sonidos", "boton.mp3"))
         if event in (None, "Salir"):
             break
         ventana.Hide()
         if event == configuracion:
-            temp = configuracion()
-            if temp != {}:
-                configuracion_seleccionada = temp
+            configuracion_temporal = configuracion()
+            if configuracion_temporal:
+                configuracion_seleccionada = configuracion_temporal
         elif event in (reglas, top_puntajes):
             event()
         elif event == jugar:
-            if partida != None:
+            if partida:
                 window_confirmar.UnHide()
                 opcion = window_confirmar.Read()[0]
                 window_confirmar.Hide()
                 ventana.UnHide()
-                ok = opcion == "Continuar"
-            if ok:
+            if opcion == "Continuar":
                 ventana.Hide()
-                partida, jugador, computadora = jugar(configuracion_seleccionada, None)
-                actualizar_top(top, jugador, computadora)
+                partida, jugador, computadora, partida_pospuesta = jugar(configuracion_seleccionada, None)
+                if not partida_pospuesta:
+                  actualizar_top(jugador, computadora, configuracion_seleccionada["nivel"])
                 if configuracion_seleccionada["nivel"] == "dificil":
                     configuracion_seleccionada["palabra valida"] = random.choice(["adjetivos", "verbos"])
         elif event == "reanudar":
-            partida, jugador, computadora = jugar(configuracion_seleccionada, partida)
-            actualizar_top(top, jugador, computadora)
+            partida, jugador, computadora, partida_pospuesta = jugar(configuracion_seleccionada, partida)
+            if not partida_pospuesta:
+              actualizar_top(jugador, computadora, configuracion_seleccionada["nivel"])
             if configuracion_seleccionada["nivel"] == "dificil":
                 configuracion_seleccionada["palabra valida"] = random.choice(["adjetivos", "verbos"])
         ventana.UnHide()
-        with open(join("componentes", "informacion_guardada", "partida_guardada"), "wb") as f:
+        if partida_pospuesta:
+          with open(join("componentes", "informacion_guardada", "partida_guardada"), "wb") as f:
             pickle.dump(partida, f)
         ventana.Element("reanudar").Update(disabled=partida == None)
 
