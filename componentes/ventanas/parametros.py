3b3620720653ba3be17c87ded8316776df1b29dd
Valentín Giorgetti <58642296+ValentinGiorgetti@users.noreply.github.com>
2020-07-28 19:07:24 -0300

    ㅤ

---
+++ componentes/ventanas/parametros.py
@@ -0,0 +1,6 @@
+titulos = {'font' : ("Consolas", 11), 'background_color' : '#1d3557', 'size' : (55, 1), 'justification' : 'center'}
+parametros_ventana = {'element_justification' : 'center', 'auto_size_text' : True, 'auto_size_buttons' : True, 'finalize' : True, 'use_default_focus' : False, 'text_justification' : 'center', 'disable_close' : True}
+
+parametros_columna = {'justification' : 'left', 'element_justification' : 'center'}
+
+parametros_popup = {"title" : "Atención", "non_blocking" : True, "auto_close_duration" : 5, "auto_close" : True}
