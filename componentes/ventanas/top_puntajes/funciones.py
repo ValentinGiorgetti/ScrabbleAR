90e60eb32958961e2a9112c16b73af944c04844a
Valentín Giorgetti <58642296+ValentinGiorgetti@users.noreply.github.com>
2020-07-28 19:07:04 -0300

    ㅤ

---
+++ componentes/ventanas/top_puntajes/funciones.py
@@ -0,0 +1,95 @@
+import pickle, PySimpleGUI as sg
+from datetime import datetime
+from componentes.jugador import Jugador
+from componentes.ventanas.parametros import parametros_ventana
+from os.path import join
+
+def crear_ventana_tops(tabla):
+
+    tamanio = (13, 1)
+
+    layout = [
+        [sg.Text("Top 10 de los mejores puntajes", size = (58,1), justification = 'center', background_color = '#1d3557')],
+        [sg.Text('')],
+        [sg.Button('General', key = 'general', button_color = ('white', 'blue'), size = tamanio), sg.Button('Nivel fácil', key = 'fácil', size = tamanio), sg.Button('Nivel medio', key = 'medio', size = tamanio), sg.Button('Nivel difícil', key = 'difícil', size = tamanio)],
+        [sg.Text('')],
+        [sg.Table(tabla, headings = ['Posición', 'Usuario', 'Puntaje', 'Fecha'], justification = 'center', key = 'top', hide_vertical_scroll = True)],
+        [sg.Text('')],
+        [sg.Button('Resetear', key = 'resetear', disabled = tabla[0][1] == '', size = tamanio), sg.Button("Volver")],
+    ]
+
+    return sg.Window("Top puntajes", layout, **parametros_ventana)
+    
+
+def leer_top():
+
+    with open(join("componentes", "informacion_guardada", "top_puntajes"), "rb") as f:
+        top = pickle.load(f)
+        
+    return top
+    
+    
+def guardar_top(top):
+
+    with open(join("componentes", "informacion_guardada", "top_puntajes"), "wb") as f:
+        pickle.dump(top, f)
+        
+        
+def generar_tabla(top):
+
+  tabla = [[i, "", "", ""] for i in range(1, 11)]
+  for i in range(len(top)):
+    tabla[i][1] = top[i][0]
+    tabla[i][2] = top[i][1]
+    tabla[i][3] = top[i][2]
+  return tabla
+        
+        
+def resetear(top, tabla, nivel, ventana_tops):
+
+    aux = f'del nivel {nivel}' if nivel != 'general' else 'general'
+    ok = sg.Popup('¿Está seguro de que quiere resetear el top ' + aux + '?', custom_text = ('Si', 'No'))
+    if ok == 'Si':
+        top[nivel] = []
+        tabla[nivel] = generar_tabla(top[nivel])
+        ventana_tops['top'].Update(values = tabla[nivel])
+        ventana_tops['resetear'].Update(disabled = True)
+        guardar_top(top)
+        
+        
+def cargar_tabla():
+
+    top = leer_top()
+    tabla = {i : generar_tabla(top[i]) for i in ['general', 'fácil', 'medio', 'difícil']}
+    return tabla
+    
+  
+def mostrar_top(ultimo_presionado, event, color, top, ventana_tops):
+
+  ventana_tops[ultimo_presionado].Update(button_color = sg.DEFAULT_BUTTON_COLOR)
+  ventana_tops[event].Update(button_color = color)
+  ventana_tops['top'].Update(values = top)
+  ventana_tops['resetear'].Update(disabled = top[0][1] == '')
+  
+  return event
+  
+
+def actualizar_top(jugador, computadora, nivel):
+    """
+    Función usada para actualizar el top de los 10 mejores puntajes.
+    """
+    
+    fecha = datetime.now().strftime('%d/%m/%Y a las %H:%M')
+
+    top = leer_top()
+
+    jugador = [("Jugador", jugador.puntaje, fecha)] if jugador.puntaje > 0 else []
+    computadora = [("Computadora", computadora.puntaje, fecha)] if computadora.puntaje > 0 else []
+    
+    temp = top['general'] + jugador + computadora
+    top['general'] = sorted(temp, key=lambda x: x[1], reverse=True)[:10]
+    
+    temp = top[nivel] + jugador + computadora
+    top[nivel] = sorted(temp, key=lambda x: x[1], reverse=True)[:10]
+
+    guardar_top(top)
---
+++ componentes/ventanas/top_puntajes/main.py
@@ -0,0 +1,31 @@
+import PySimpleGUI as sg
+from playsound import playsound as reproducir
+from componentes.ventanas.top_puntajes.funciones import *
+
+def main():
+    """
+    Función que muestra una ventana con el top 10 de los mejores puntajes.
+    Abre un archivo binario que contiene una lista de tuplas ('Computadora / Jugador', Objeto jugador) ordenada
+    por puntaje en forma descendente. Lo que hace es mostrar esa información en la ventana.
+    """
+    
+    colores = {'general' : ('white', 'blue'), 'fácil' : ("white", "green"), 'medio' : ("white", "orange"), 'difícil' : ('white', 'red')}
+    
+    ultimo_presionado = 'general'
+    
+    top = leer_top()
+        
+    tabla = cargar_tabla()
+
+    window = crear_ventana_tops(tabla['general'])
+    
+    while True:
+      event, values = window.Read()
+      reproducir(join("componentes", "sonidos", "boton.mp3"))
+      if event in (None, 'Volver'):
+        break
+      elif event == 'resetear':
+        resetear(top, tabla, ultimo_presionado, window)
+      else:
+        ultimo_presionado = mostrar_top(ultimo_presionado, event, colores[event], tabla[event], window)
+    window.Close()
