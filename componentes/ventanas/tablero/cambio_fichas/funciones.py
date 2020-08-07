eae0591ded0a69882431950789abeb52b70351a2
Valentín Giorgetti <58642296+ValentinGiorgetti@users.noreply.github.com>
2020-08-06 23:13:08 -0300

    Update funciones.py

--- componentes/ventanas/tablero/cambio_fichas/funciones.py
+++ componentes/ventanas/tablero/cambio_fichas/funciones.py
@@ -1,4 +1,7 @@
 """
+Autores: Garea Antonella, Garofalo Pedro y Giorgetti Valentín
+Licencia: GNU General Public License
+
 Módulo que contiene las funciones usadas en la ventana de cambio de fichas.
 """
 
