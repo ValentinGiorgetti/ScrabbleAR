052e24207d31a840d5d48626f1880bd5b40dd522
Valentín Giorgetti <58642296+ValentinGiorgetti@users.noreply.github.com>
2020-09-07 21:25:20 -0300

    ㅤ

--- componentes/ventanas/general.py
+++ componentes/ventanas/general.py
@@ -6,38 +6,40 @@ Módulo que contiene parámetros usados para la creación de ventanas y una func
 """
 
 
+import time
 from os.path import join
 from playsound import playsound as reproducir
-import time
 
 
-titulos = {'font' : ("Consolas", 11), 
-           'background_color' : '#1d3557', 
-           'size' : (55, 1), 
-           'justification' : 'center'}
+titulos = {"font" : ("Consolas", 12), 
+           "background_color" : "#1d3557", 
+           "justification" : "center"}
            
-parametros_ventana = {'element_justification' : 'center', 
-                      'auto_size_text' : True, 
-                      'auto_size_buttons' : True, 
-                      'finalize' : True, 
-                      'use_default_focus' : False, 
-                      'text_justification' : 'center', 
-                      'disable_close' : True}
+parametros_ventana = {"element_justification" : "center", 
+                      "auto_size_text" : True, 
+                      "auto_size_buttons" : True, 
+                      "finalize" : True, 
+                      "use_default_focus" : False, 
+                      "text_justification" : "center"}
 
-parametros_columna = {'justification' : 'left', 
-                      'element_justification' : 'center'}
+parametros_columna = {"justification" : "left", 
+                      "element_justification" : "center"}
 
-parametros_popup = {"title" : "  Atención", 
+parametros_popup = {"title" : " Atención", 
                     "non_blocking" : True, 
-                    "auto_close_duration" : 5, 
-                    "auto_close" : True}
+                    "auto_close_duration" : 8, 
+                    "auto_close" : True,
+                    "custom_text" : (" Aceptar ", None)}
+                    
+cartel = {"title" : " Atención", "custom_text" : (" Aceptar ", None)}
 
-colores = {'Fácil' : ("white", "green"), 
-           'Medio' : ("white", "orange"), 
-           'Difícil' : ('white', 'red')}
+colores = {"Fácil" : ("white", "green"), 
+           "Medio" : ("white", "orange"), 
+           "Difícil" : ("white", "red"),
+           "General" : ("white", "blue")}
                     
                     
-def leer_evento(window, tiempo = None, key = ''):
+def leer_evento(window, tiempo = None, key = ""):
     """
     Función usada para leer un evento. 
     
@@ -50,13 +52,15 @@ def leer_evento(window, tiempo = None, key = ''):
         - key: evento a retornar si se llega al timeout.
         
     Retorna:
-        - el evento recibido.
+        - el evento leído.
         - (dict): los valores de los widgets de la ventana.
         - (int): tiempo transcurrido hasta que se leyó un evento.
     """
     
     inicio = time.time()
+    
     event, values = window.Read(timeout = tiempo, timeout_key = key)  
+    
     if (event != key):
         reproducir(join("componentes", "sonidos", "boton.mp3"))
         
